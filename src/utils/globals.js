cab90e0799dae91170e270b923c596f66e480b92
tristan <tristanjhonmuyot@gmail.com>
2024-01-05 23:49:57 -0800

    apiupdate

--- src/utils/globals.js
+++ src/utils/globals.js
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = "https://car-rental-backend-ruby.vercel.app/api";
-export const API_BASE_URL = "http://localhost:3000/api";
+export const API_BASE_URL = "https://car-rental-backend-ruby.vercel.app/api";
+// export const API_BASE_URL = "http://localhost:3000/api";
